571877b42c721d343d48231c74a153c42a32d6f0
ecb <ecb@hostname.hep.manchester.ac.uk>
2018-05-21 13:14:23 +0100

    added log mode

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
  
     Int_t num_bins{10};
-    CanvasFactorySettings s("x_axis", "y_axis", 0.0, 5.0, false);
+    CanvasFactorySettings s("x_axis", "y_axis", 0.1, 5.0, true);
     //s.SetCanvasSize(804, 628); // TODO: without this does not work!
     // TODO: colors not working
     //s.SetAxisTitleFont(4 * 10 + 2); // default 4, 5 or something else?
@@ -21,7 +21,7 @@ int main()
     h_el_energy_original->Fill(3.0);
     h_el_energy_original->Fill(3.0);
     h_el_energy_reweight->Fill(1.0, 2.0);
-    f.Canvas("el_energy", "el_energy_nolog_dir", h_el_energy_original, "Baseline", h_el_energy_reweight, "Reweighted");
+    f.Canvas("el_energy", "el_energy_log_dir", h_el_energy_original, "Baseline", h_el_energy_reweight, "Reweighted");
 
     return 0;
 }
